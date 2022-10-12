02d9e35c31a0def90fcbc482c3da9616a2aa8b6f
Yen-Chi Chen <zxkyjimmy@gmail.com>
2022-10-13 02:12:40 +0800

    shorten cudnn file name

--- setup.sh
+++ setup.sh
@@ -71,7 +71,7 @@ echo $'export PATH="/usr/local/cuda/bin:$PATH"\n' >> $HOME/.zshrc
 
 step "Get cudnn"
 wget https://developer.download.nvidia.com/compute/redist/cudnn/v8.6.0/local_installers/11.8/cudnn-linux-x86_64-8.6.0.163_cuda11-archive.tar.xz
-tar -xvf cudnn-linux-*-archive.tar.xz
+tar -xvf cudnn-*-archive.tar.xz
 sudo cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include
 sudo cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
