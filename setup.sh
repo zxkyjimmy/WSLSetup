abdd5e114e09d9c344bb71f76b0ac13b5709f25d
Yen-Chi Chen <zxkyjimmy@gmail.com>
2022-10-13 00:47:17 +0800

    export LD_LIBRARY_PATH

--- setup.sh
+++ setup.sh
@@ -76,6 +76,7 @@ sudo cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include
 sudo cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
 sudo ldconfig
+echo $'export LD_LIBRARY_PATH="/usr/lib/wsl/lib:$LD_LIBRARY_PATH"\n' >> $HOME/.zshrc
 
 step "Install Podman"
 sudo apt update
